c700fcc83e494bed4d228b564f035fdaf2dc3c99
Dhruv Sharma <134800442+dhruvviee@users.noreply.github.com>
2024-07-31 18:08:06 +0530

    initial commit

---
+++ Next-iOS/App/Next_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Next_iOSApp.swift
+//  Next-iOS
+//
+//  Created by Dhruv Sharma on 31/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Next_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Next-iOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Next-iOS
+//
+//  Created by Dhruv Sharma on 31/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
